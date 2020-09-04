340c5103cf266b1e85627238c9d414f45be73a14
sai1004 <saikiran.bavandla@gmail.com>
2020-09-04 19:26:38 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RegisterPageComponent } from "./register-page/register-page.component";
+import { DashboardPageComponent } from "./dashboard-page/dashboard-page.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", component: RegisterPageComponent },
+    { path: "dashboard", component: DashboardPageComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { Ng2TelInputModule } from "ng2-tel-input";
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegisterPageComponent, DashboardPageComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, Ng2TelInputModule],
   providers: [],
   bootstrap: [AppComponent],
